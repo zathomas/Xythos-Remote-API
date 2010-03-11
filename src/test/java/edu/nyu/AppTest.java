300741d552d6335336090b1e3db27689334c751e
Zach A. Thomas <zach@aeroplanesoftware.com>
2010-03-11 15:37:07 -0600

    removing dependencies on Spring that I don't use anymore

--- src/test/java/edu/nyu/AppTest.java
+++ src/test/java/edu/nyu/AppTest.java
@@ -1,15 +1,8 @@
 package edu.nyu;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration
 public class AppTest {
-	
-	@Autowired
-	private XythosRemote xythosService;
-	
 	@Test
 	public void testTruth() {
 		assert(true);
